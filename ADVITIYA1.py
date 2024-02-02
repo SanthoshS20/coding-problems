041b3fb30bc3ea6e816d5f88ef24925702f7c9de
Santhosh S <santhoshsekar97@gmail.com>
2024-02-02 10:46:05 +0530

    Update ADVITIYA1.py

--- ADVITIYA1.py
+++ ADVITIYA1.py
@@ -6,25 +6,22 @@
 
 ADVITIYA_DATES = [16, 17, 18]
 
-def decideParticipantStatus(date, status=None):
+def decide_participant_status(date):
   """Decides the participant visiting date is same as one of the advitiya fest dates
 
     Args:
       date (int): The visiting date of a participant
-      status (:obj:`bool`, optional): Optional parameter
     
     Returns:
       bool: True if the participant visiting date is on the advitiya fest dates otherwise false
       
   """
-  if date in ADVITIYA_DATES:
-    return True
-  else:
-    return False
+  return (date in ADVITIYA_DATES)
+
 
 if __name__ == "__main__":
   visitingDate = int(input())
-  mehulStatus = decideParticipantStatus(visitingDate)
+  mehulStatus = decide_participant_status(visitingDate)
   if(mehulStatus):
     print("ADVITIYA")
   else:
